2955efbcedc8ea583f1acb8f375241236acc0fba
thanhtunguet <thanhtung.uet@gmail.com>
2025-03-29 16:58:12 +0700

    crawl concurrently

--- src/crawler/crawler-service/crawl-all.ts
+++ src/crawler/crawler-service/crawl-all.ts
@@ -11,19 +11,27 @@ export async function crawlAll(
   const provinces = await this.getProvinces();
 
   const provinceCount = provinces.length;
+
   let completed = 0;
 
-  for (const { name } of provinces) {
-    try {
-      await this.crawlProvince(job, name, progressCb);
-      await sleep(Math.random() * SLEEP_GAP + SLEEP_MIN);
-      completed++;
-      const progress = parseFloat(
-        ((completed / provinceCount) * 100).toFixed(2),
-      );
-      await progressCb(job, progress);
-    } catch (error) {
-      console.error(`Error crawling province ${name}`, error);
-    }
+  const MAX_CONCURRENT_REQUESTS = 4;
+
+  for (let i = 0; i < provinceCount; i += MAX_CONCURRENT_REQUESTS) {
+    const chunks = provinces.slice(i, i + MAX_CONCURRENT_REQUESTS);
+    await Promise.all(
+      chunks.map(async ({ name }) => {
+        try {
+          await this.crawlProvince(job, name, progressCb);
+          completed++;
+          const progress = parseFloat(
+            ((completed / provinceCount) * 100).toFixed(2),
+          );
+          await progressCb(job, progress);
+        } catch (error) {
+          console.error(`Error crawling province ${name}`, error);
+        }
+      }),
+    );
+    await sleep(Math.random() * SLEEP_GAP + SLEEP_MIN);
   }
 }
